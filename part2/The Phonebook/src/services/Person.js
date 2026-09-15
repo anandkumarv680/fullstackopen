13db9ca810bdc3b06213533fdb5869f248a8d9fc
Anand kumar verma <anandkumarv680@gmail.com>
2026-09-15 14:45:02 +0530

    complete part3b

--- part2/The Phonebook/src/services/Person.js
+++ part2/The Phonebook/src/services/Person.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-
-const baseUrl = "http://localhost:3001/api/persons";
+const baseUrl = import.meta.env.DEV
+  ? "http://localhost:3001/api/persons"
+  : "/api/persons";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
