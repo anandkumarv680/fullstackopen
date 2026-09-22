67fcca0a5ec104cb844c68949bf968a134506786
Anand kumar Verma <anandkumarv680@gmail.com>
2026-09-22 22:47:56 +0530

    complete part 4b

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -2,80 +2,34 @@ const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 
-
-blogRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogRouter.post('/', (request, response) => {
+blogRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
+  const result = await blog.save()
+  response.status(201).json(result)
 })
-// notesRouter.get('/', (request, response) => {
-//   Note.find({}).then(notes => {
-//     response.json(notes)
-//   })
-// })
 
-// notesRouter.get('/:id', (request, response, next) => {
-//   Note.findById(request.params.id)
-//     .then(note => {
-//       if (note) {
-//         response.json(note)
-//       } else {
-//         response.status(404).end()
-//       }
-//     })
-//     .catch(error => next(error))
-// })
+blogRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
 
-// notesRouter.post('/', (request, response, next) => {
-//   const body = request.body
+  response.status(204).end()
+})
 
-//   const note = new Note({
-//     content: body.content,
-//     important: body.important || false,
-//   })
+blogRouter.put('/:id', async (request, response) => {
+  const { likes } = request.body
 
-//   note.save()
-//     .then(savedNote => {
-//       response.json(savedNote)
-//     })
-//     .catch(error => next(error))
-// })
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    { likes },
+    { new: true, runValidators: true }
+  )
 
-// notesRouter.delete('/:id', (request, response, next) => {
-//   Note.findByIdAndDelete(request.params.id)
-//     .then(() => {
-//       response.status(204).end()
-//     })
-//     .catch(error => next(error))
-// })
-
-// notesRouter.put('/:id', (request, response, next) => {
-//   const { content, important } = request.body
-
-//   Note.findById(request.params.id)
-//     .then(note => {
-//       if (!note) {
-//         return response.status(404).end()
-//       }
-
-//       note.content = content
-//       note.important = important
-
-//       return note.save().then((updatedNote) => {
-//         response.json(updatedNote)
-//       })
-//     })
-//     .catch(error => next(error))
-// })
+  response.json(updatedBlog)
+})
 
 module.exports = blogRouter
-
-
---
+++ part4/tests/blogs_api.test.js
@@ -0,0 +1,132 @@
+const { test } = require('node:test')
+const assert = require('node:assert')
+const supertest = require('supertest')
+
+const app = require('../app')
+
+const api = supertest(app)
+
+test('blogs are returned as json', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.length, 5)
+})
+
+test('blogs have an id property', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogs = response.body
+
+  assert(blogs.every(blog => blog.id))
+})
+
+test('a new blog can be added', async () => {
+  const blogsAtStart = await api.get('/api/blogs')
+
+  const newBlog = {
+    title: 'Testing with SuperTest',
+    author: 'Test Author',
+    url: 'http://example.com',
+    likes: 5
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await api.get('/api/blogs')
+
+  assert.strictEqual(blogsAtEnd.body.length, blogsAtStart.body.length + 1)
+})
+
+test('likes defaults to 0 if missing', async () => {
+  const newBlog = {
+    title: 'Blog without likes',
+    author: 'Test Author',
+    url: 'http://example.com'
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.likes, 0)
+})
+
+test('blog without title is not added', async () => {
+  const newBlog = {
+    author: 'Test Author',
+    url: 'http://example.com',
+    likes: 5
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+})
+
+test('blog without url is not added', async () => {
+  const newBlog = {
+    title: 'Blog without URL',
+    author: 'Test Author',
+    likes: 5
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+})
+
+
+test('a blog can be deleted', async () => {
+  const blogsAtStart = await api.get('/api/blogs')
+
+  const blogToDelete = blogsAtStart.body[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await api.get('/api/blogs')
+
+  assert.strictEqual(
+    blogsAtEnd.body.length,
+    blogsAtStart.body.length - 1
+  )
+
+  assert(
+    blogsAtEnd.body.every(blog => blog.id !== blogToDelete.id)
+  )
+})
+
+
+test('a blog can be updated', async () => {
+  const blogsAtStart = await api.get('/api/blogs')
+
+  const blogToUpdate = blogsAtStart.body[0]
+
+  const updatedBlog = {
+    ...blogToUpdate,
+    likes: blogToUpdate.likes + 1
+  }
+
+  const response = await api
+    .put(`/api/blogs/${blogToUpdate.id}`)
+    .send(updatedBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.likes, blogToUpdate.likes + 1)
+})
